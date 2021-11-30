cf2c9d61ac90fc09d3319ab2898ec9dfeec50501
kais manai <kaismanai1975@gmail.com>
2021-11-30 12:43:55 +0100

    Fetching data

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,19 +1,19 @@
-import React from 'react'
+import React from "react";
 
 function Header() {
-    return (
-      
-            <header className="row">
+  return (
+    <header className="row">
       <div>
-        <a className="brand" href="/">ManShop</a>
+        <a className="brand" href="/">
+          ManShop
+        </a>
       </div>
       <div>
         <a href="/cart">Cart</a>
         <a href="/signin">Sign In</a>
       </div>
     </header>
-        
-    )
+  );
 }
 
-export default Header
+export default Header;
